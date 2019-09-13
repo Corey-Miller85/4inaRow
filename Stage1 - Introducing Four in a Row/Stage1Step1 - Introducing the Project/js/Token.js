8c276ae28ea6638bbab87abc2ecf7f4136353709
Corey Miller <coreymiller353@gmail.com>
2019-09-13 16:31:55 -0400

    add player initializ

--- Stage1 - Introducing Four in a Row/Stage1Step1 - Introducing the Project/js/Token.js
+++ Stage1 - Introducing Four in a Row/Stage1Step1 - Introducing the Project/js/Token.js
@@ -1,5 +1,8 @@
 class Token {
-    constructor() {
-        
+    constructor(index, owner) {
+        this.owner = owner;
+        this.id = `token-${index}-${owner.id}`;
+        this.dropped = false;
+
     }
 }
